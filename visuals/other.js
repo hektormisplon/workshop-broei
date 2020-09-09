cade842a8de35276c6b0ef9500f66a0682af1cd0
Hektor Misplon <hektor.misplon@gmail.com>
2020-09-09 18:58:47 +0200

    Add some 'other' stuff

--- visuals/other.js
+++ visuals/other.js
@@ -1,5 +1,20 @@
 /*
- * Extra source
+ * Gradient: extra source
  */
 
 gradient().out()
+
+/*
+ * Shape optional parameters (radius??? & smoothing)
+ */
+
+shape(4,() => Math.sin(1 * time), 1).out()
+
+/*
+ * Parameter sequencing
+ */
+
+osc([1,10,100]).out()
+osc([1,10,100].fast(2)).out()
+
+solid(1,1,1).out()
